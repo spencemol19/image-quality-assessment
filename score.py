32589309c7a9ce9f9efc477fffdff3b0b7cd13e6
Spencer Moulton <spencermoulton@Spencers-MacBook-Pro.local>
2019-06-07 14:37:05 -0600

    ready to run score-class generation for svm feature prep

--- score.py
+++ score.py
@@ -11,18 +11,26 @@ IQA_TYPES = {
     'technical': '{pwd}/models/MobileNet/weights_mobilenet_technical_0.11.hdf5'
 }
 
+SVM_CLASS_MAP = {
+    1: 'high',
+    2: 'medium',
+    3: 'low'
+}
+
 IQA_WEIGHTS = IQA_TYPES['technical']
 
 '''
     SAMPLE CL CALL:
     
-    python score.py -f /home/spencer/quality/image-quality-assessment/banjo_data/blur-detection-set {-a} -t 3 5 7 10
+    python score.py -f /home/spencer/quality/image-quality-assessment/banjo_data/blur-detection-set {-a -c 1/2/3 -s} -t 3 5 7 10
     
     ANY -[a-z] args are also accepted as -[A-Z]
     
     {} - indicates optional args segments
     
     -a = Use Aesthetic NIMA IQA (not Technical)
+    -c = SVM Class (1+)
+    -s = "Silent" mode to not output basic statistics for each processed dataset
     
     MUST use -f first and -t last followed by their respective args values in script execution call
     
@@ -51,6 +59,27 @@ def main(args):
     '''
     iqa_weights = IQA_WEIGHTS
     threshold_vals = []
+    svm_class = -1
+    is_silent = False
+
+    # class arg
+    if '-c' in [a.lower() for a in args]:
+        class_ind = [a.lower() for a in args].index('-c')
+
+        try:
+            svm_class = int(args[class_ind + 1])
+        except ValueError:
+            pass
+
+        args = args[:class_ind] + args[class_ind + 2:]
+
+    # run silently arg ( will not show stats for given dataset folder(s) )
+    if '-s' in [a.lower() for a in args]:
+        assess_ind = [a.lower() for a in args].index('-s')
+
+        is_silent = True
+
+        args = args[:assess_ind] + args[assess_ind + 1:]
 
     # use aesthetic for IQA type (optional arg)
     if '-a' in [a.lower() for a in args]:
@@ -67,8 +96,14 @@ def main(args):
     # check for threshold(s) REQUIRED
     if '-t' in [a.lower() for a in args]:
         threshold_ind = [a.lower() for a in args].index('-t')
-        threshold_vals = sorted([float(v) for v in args[threshold_ind + 1:]])
+        try:
+            threshold_vals = sorted([float(v) for v in args[threshold_ind + 1:]])
+        except:
+            pass
+
         args = args[:threshold_ind]
+
+    dir_counter = 0
 
     if args[0].lower().replace('-', '') == 'f':
         cli_exec_str = get_predict_cli_str(weights_file=iqa_weights)
@@ -94,32 +129,44 @@ def main(args):
 
                 end_time = time.time()
 
-                print('\n%s\n\nprocessed %d in %f s for "%s" data' % (
-                    '-' * 60, len(aggr_scores), end_time - start_time, os.path.basename(dir)))
+                if not is_silent:
+                    print('\n%s\n\nprocessed %d in %f s for "%s" data' % (
+                        '-' * 60, len(aggr_scores), end_time - start_time, os.path.basename(dir)))
 
-                total_valid = []
+                    total_valid = []
 
-                for idx, threshold_val in enumerate(threshold_vals):
-                    prev = 0 if idx == 0 else threshold_vals[idx - 1]
-                    valid = [s for s in aggr_scores if check_within_range(s, prev, threshold_val)]
-                    total_valid.extend(valid)
-                    print('\nFound %d/%d in [%f, %f]' % (len(valid), len(aggr_scores), prev, threshold_val))
+                    for idx, threshold_val in enumerate(threshold_vals):
+                        prev = 0 if idx == 0 else threshold_vals[idx - 1]
+                        valid = [s for s in aggr_scores if check_within_range(s, prev, threshold_val)]
+                        total_valid.extend(valid)
+                        print('\nFound %d/%d in [%f, %f]' % (len(valid), len(aggr_scores), prev, threshold_val))
 
-                neg_outliers = [s for s in aggr_scores if s < 0]
+                    neg_outliers = [s for s in aggr_scores if s < 0]
 
-                pos_outliers = [s for s in aggr_scores if s > threshold_vals[-1]]
+                    pos_outliers = [s for s in aggr_scores if s > threshold_vals[-1]]
 
-                print('\nFound %d/%d negative outliers' % (
-                    len(neg_outliers), len(aggr_scores)))
+                    print('\nFound %d/%d negative outliers' % (
+                        len(neg_outliers), len(aggr_scores)))
 
-                print('\nFound %d/%d positive outliers' % (
-                    len(pos_outliers), len(aggr_scores)))
+                    print('\nFound %d/%d positive outliers' % (
+                        len(pos_outliers), len(aggr_scores)))
 
-                print('\nMean: %f' % (sum(aggr_scores) / len(aggr_scores)))
-                print('\nStandard deviation: %f\n' % statistics.stdev(aggr_scores))
+                    print('\nMean: %f' % (sum(aggr_scores) / len(aggr_scores)))
+                    print('\nStandard deviation: %f\n' % statistics.stdev(aggr_scores))
 
-                hist = np.histogram(aggr_scores)
-                print('\nHistogram: ', hist)
+                    hist = np.histogram(aggr_scores)
+
+                    print('\nHistogram: ', hist)
+
+                if svm_class > -1:
+                    svm_term = SVM_CLASS_MAP[svm_class]
+                    with open('svm_data/{quality}.csv'.format(quality=svm_term), 'a') as csv_src:
+                        csv_src.writelines(['%s,%s\n' % (s, svm_class) for s in aggr_scores])
+
+                    dir_counter += len(aggr_scores)
+
+        if dir_counter > 0:
+            print('Classified %d score for quality class: %s' % (dir_counter, svm_term))
 
 
 if __name__ == '__main__':
--- test_banjo_data.py
+++ test_banjo_data.py
@@ -1,4 +1,5 @@
 import subprocess
+import json
 import time
 import sys
 import os
@@ -13,35 +14,70 @@ DATA_DIRS_CMD = 'find ./banjo_data/* -mindepth 0 -maxdepth {max_depth} -type d'.
     max_depth=MAX_DEPTH_DATA_SET_DIRS)
 
 
-def main(args):
+def run_model_test_batch(args, data_dirs=None):
     exec_base = ['python', 'score.py', '-f']
 
-    data_dirs = subprocess.check_output(DATA_DIRS_CMD, shell=True).decode('utf-8', errors='ignore').splitlines()
+    if not data_dirs:
+        data_dirs = subprocess.check_output(DATA_DIRS_CMD, shell=True).decode('utf-8', errors='ignore').splitlines()
 
     dir_names = [os.path.abspath(_dir) for _dir in data_dirs]
 
     exec_base.extend(dir_names)
 
-    # if gpu spec flag is passed in, forward it
-    if '-g' in [a.lower() for a in args]:
-        exec_base.append('-g')
+    lower_args = [a.lower() for a in args]
+
+    is_silent = False
 
     # if aesthetic iqa flag is passed in, forward it
-    if '-a' in [a.lower() for a in args]:
+    if '-a' in lower_args:
         exec_base.append('-a')
+
+    # if silent flag
+    if '-s' in lower_args:
+        is_silent = True
+        exec_base.append('-s')
+
+    if '-c' in lower_args:
+        base_idx = lower_args.index('-c')
+        class_val = lower_args[base_idx + 1]
+        if '-' not in class_val:
+            exec_base.append('-c')
+            exec_base.append(class_val)
 
     exec_base.append('-t')
 
     exec_base.extend([str(t) for t in THRESHOLDS])
 
-    out_file = './banjo-test/banjo_data_model_performance.%d.txt' % int(time.time())
-
     print(exec_base)
 
-    testing_output = subprocess.check_output(exec_base).decode('utf-8', errors='ignore').strip()
+    return
 
-    with open(out_file, 'w') as out_fi:
-        out_fi.write(testing_output)
+    if is_silent:
+        subprocess.call(exec_base)
+    else:
+        out_file = './banjo-test/banjo_data_model_performance.%d.txt' % int(time.time())
+
+        testing_output = subprocess.check_output(exec_base).decode('utf-8', errors='ignore').strip()
+
+        with open(out_file, 'w') as out_fi:
+            out_fi.write(testing_output)
+
+
+def main(args):
+    if '--config' in args:
+        config_idx = args.index('--config')
+        config_file = args[config_idx + 1]
+        if os.path.isfile(config_file):
+            with open(config_file) as config_src:
+                raw_classes = json.load(config_src)
+                for idx, _class in enumerate(raw_classes):
+                    idx += 1
+                    data_dirs = [os.path.join(DATA_PATH, d) for d in _class]
+                    c_args = args[:config_idx] + ['-s', '-c', str(idx)]
+                    run_model_test_batch(c_args, data_dirs)
+
+    else:
+        run_model_test_batch(args)
 
 
 if __name__ == '__main__':
